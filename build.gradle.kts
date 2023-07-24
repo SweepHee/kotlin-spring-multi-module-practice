e4b21c31e60595adefb4bf923b17c83777fd4b18
Sweep Hee <jeonsh1991@gmail.com>
2023-07-24 23:40:40 +0900

    :label: first commit

---
+++ build.gradle.kts
@@ -0,0 +1,85 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    java
+    id("org.springframework.boot") version "2.7.14" apply false
+    id("io.spring.dependency-management") version "1.1.2" apply false
+    kotlin("jvm") version "1.8.21"
+    kotlin("plugin.spring") version "1.8.21" apply false
+    kotlin("plugin.jpa") version "1.8.21"
+    kotlin("plugin.noarg") version "1.8.21"
+    kotlin("plugin.allopen") version "1.8.21"
+
+}
+
+
+allprojects{
+    group = "com.example"
+    version = "0.0.1-SNAPSHOT"
+
+    tasks.withType<JavaCompile>{
+        sourceCompatibility = "11"
+        targetCompatibility = "11"
+    }
+
+    tasks.withType<KotlinCompile> {
+        kotlinOptions {
+            freeCompilerArgs = listOf("-Xjsr305=strict")
+            jvmTarget = "11"
+        }
+    }
+
+    tasks.withType<Test> {
+        useJUnitPlatform()
+    }
+
+    repositories {
+        mavenCentral()
+    }
+}
+
+subprojects{
+
+    apply(plugin = "org.springframework.boot")
+    apply(plugin = "io.spring.dependency-management")
+    apply(plugin = "org.jetbrains.kotlin.plugin.spring")
+    apply(plugin = "kotlin")
+
+
+
+
+    dependencies {
+        implementation("org.springframework.boot:spring-boot-starter-web")
+        testImplementation("org.springframework.boot:spring-boot-starter-test")
+    }
+
+
+}
+
+
+project(":core") {
+
+    apply(plugin = "kotlin-allopen")
+    apply(plugin = "kotlin-noarg")
+
+    allOpen {
+        annotation("jakarta.persistence.Entity")
+        annotation("jakarta.persistence.Embeddable")
+        annotation("jakarta.persistence.MappedSuperclass")
+    }
+
+    noArg {
+        annotation("jakarta.persistence.Entity")
+        annotation("jakarta.persistence.Embeddable")
+        annotation("jakarta.persistence.MappedSuperclass")
+    }
+
+    dependencies {
+        implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+        implementation("org.springframework.boot:spring-boot-starter-jooq")
+        implementation("org.jetbrains.kotlin:kotlin-reflect")
+        runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
+
+    }
+
+}
---
+++ core/src/main/java/com/example/multimodule/app.kt
@@ -0,0 +1,10 @@
+package com.example.multimodule
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+@SpringBootApplication
+class App
+
+fun main(args: Array<String>) {
+    runApplication<App>(*args)
+}
